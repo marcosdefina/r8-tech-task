5b22175900b86502735b715c6bf32cf821b19b21
Marcos Defina <marcosdefina@gmail.com>
2019-07-12 20:42:49 +0300

    simplifying the code

--- index.js
+++ index.js
@@ -1,87 +1,48 @@
-TESTER = document.getElementById('tester');
-
-var firstX = [window.datasets['15360'].length];
-var firstY = [window.datasets['15360'].length];
-var secondX = [];
-var secondY = [];
-var thirdX = [];
-var thirdY = [];
-var fourthX = [];
-var fourthY = [];
-
-var firstDataSet = window.datasets['15360'];
-var secondDataSet = window.datasets['15361'];
-var thirdDataSet = window.datasets['15377'];
-var fourthDataSet = window.datasets['17006'];
-
-
-function desserializeData(x, y, dataset){
+function desserializeData(dataset){
+  var data = [];
   var i = 0;
 
   while(i < dataset.length){
-    x[i] = timeConverter(dataset[i]["0"]);
-    y[i] = dataset[i]["1"];
+    data[i].push(timeConverter(dataset[i]["0"]));
+    data[i].push(dataset[i]["1"]);
     i++;
   }
+  return data;
 }
 
 function timeConverter(time){
   return new Date(time).toString();
 }
 
-desserializeData(firstX, firstY, firstDataSet); 
-desserializeData(secondX, secondY, secondDataSet); 
-desserializeData(thirdX, thirdY, thirdDataSet); 
-desserializeData(fourthX, fourthY, fourthDataSet); 
+function plot(data, dom){
+  Plotly.plot( dom, 
+      [{
+        x: data[0], y:data[0]
+      }],
+      { 
+        margin: { t: 0 }
+      },
+      {showSendToCloud:true}
+  );
+}
 
-firstGraph = document.getElementById('first');
-secondGraph = document.getElementById('second');
-thirdGraph = document.getElementById('third');
-fourthGraph = document.getElementById('fourth');
+var firstDataSet = window.datasets['15360'];
+var secondDataSet = window.datasets['15361'];
+var thirdDataSet = window.datasets['15377'];
+var fourthDataSet = window.datasets['17006'];
+var datasets = [firstDataSet, secondDataSet, thirdDataSet, fourthDataSet];
 
-Plotly.plot( firstGraph, 
-    [{
-      x: firstX, y:firstY
-    }],
-    { 
-      margin: { t: 0 }
-    },
-    {showSendToCloud:true}
-);
+var data;
+for(var dataset in datasets){
+  data.push(desserializeData(dataset));
+} 
 
-Plotly.plot( secondGraph, 
-    [{
-      x: secondX, y: secondY
-    }], 
-    { 
-      margin: { t: 0 }
-    },
-    {showSendToCloud:true}
-);
+var firstGraph = document.getElementById('first');
+var secondGraph = document.getElementById('second');
+var thirdGraph = document.getElementById('third');
+var fourthGraph = document.getElementById('fourth');
+var graphs = [firstGraph, secondGraph, thirdGraph, fourthGraph];
 
-Plotly.plot( thirdGraph, 
-    [{
-      x: thirdX, y: thirdY
-    }], 
-    { 
-      margin: { t: 0 }
-    },
-    {showSendToCloud:true}
-);
-Plotly.plot( fourthGraph, 
-    [{
-      x: fourthX, y: fourthY
-    }], 
-    { 
-      margin: { t: 0 }
-    },
-    {showSendToCloud:true}
-);
-
-/* Current Plotly.js version */
-
-console.log("firstX:")
-console.log(firstX)
-console.log("firstY:")
-console.log(firstY)
-console.log(window.datasets['15360'])
+for(var graph in graphs){
+  plot(data[graphs.indexOf(graph)] ,graph);
+}
